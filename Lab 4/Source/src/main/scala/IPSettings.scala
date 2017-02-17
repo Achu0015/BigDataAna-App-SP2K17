c300f269775ffba4fa4bbb9405f0c29ee86c8b5f
Achu0015 <nar.achualt@gmail.com>
2017-02-16 22:46:28 -0600

    Lab 4

---
+++ Lab 4/Source/src/main/scala/IPSettings.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by Achu on 2/16/15.
+  */
+object IPSettings {
+
+  val PATH = "data/"
+  val INPUT_DIR = PATH + "train"
+  val TEST_INPUT_DIR = PATH + "test"
+  val FEATURES_PATH = PATH + "model/features"
+
+  val KMEANS_PATH = PATH + "model/clusters"
+  val KMEANS_CENTERS_PATH = PATH + "model/clusterCenters"
+  val HISTOGRAM_PATH = PATH + "model/histograms"
+  val RANDOM_FOREST_PATH = PATH + "model/nbmodel"
+  val DECISION_TREE = PATH + "model/nbmodel"
+}
